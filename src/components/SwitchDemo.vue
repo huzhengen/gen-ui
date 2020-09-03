d6d44d8fdd6b2c4c56e6a05d1fcb11a4baaa7c6c
hukai <huzhengen@163.com>
2020-09-03 11:11:20 +0800

    Switch

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>switch</div>
+  <Switch v-model:value="bool" />
 </template>
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
+export default {
+  components: { Switch },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
+};
+</script>
