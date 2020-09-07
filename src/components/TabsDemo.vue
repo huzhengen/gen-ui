16f00ba0bd69d3d3474851533b8ac30b87835562
hukai <huzhengen@163.com>
2020-09-07 16:15:32 +0800

    Tabs

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>Tabs 示例</div>
+  <div>Examples</div>
+  <h1>Example 1</h1>
+  <Tabs v-model:selected="x">
+    <Tab title="Tab1">Content 1</Tab>
+    <Tab title="Tab2">Content 2</Tab>
+  </Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  setup() {
+    const x = ref("Tab1");
+    return {
+      x,
+    };
+  },
+};
+</script>
